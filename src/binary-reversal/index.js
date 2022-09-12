2d2d8c0ede61ff59f250f0164e6e79b793b64116
Kunlexi <91717420+Kunlexi@users.noreply.github.com>
2022-09-12 21:29:16 +0100

    updae

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,16 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
-
+function binaryReversal(str) {
+    const binaryValue = Number(str).toString(2);
+    const paddingLength = binaryValue.length % 8 === 0 ? 0 : 8 - binaryValue.length % 8
+    const paddedReversedBinaryValue = [
+      ...Array(paddingLength).fill(0), 
+      ...binaryValue]
+      .reverse()
+      .join('');
+  
+    return parseInt(paddedReversedBinaryValue,2);
+}
+console.log(binaryReversal());
 module.exports = binaryReversal;
